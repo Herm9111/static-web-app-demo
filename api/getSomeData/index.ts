759dce9d51ca133e809e650c2aedc1f984d65e86
Mau <herm9111@gmail.com>
2022-03-29 14:47:28 -0700

    added common module to api

--- api/getSomeData/index.ts
+++ api/getSomeData/index.ts
@@ -1,8 +1,11 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { ISomeData } from '../common/interfaces/some-data-interface';
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const responseMessage = 'SomeDATA';
+    const responseMessage: ISomeData = {
+        data:'SomeDATA',
+        number: 1
+    };
 
     context.res = {
         // status: 200, /* Defaults to 200 */
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ISomeData } from '../api/common/interfaces/some-data-interface';
 
 function App() {
-  const [data, setData] = setState();
+  const [data, setData] = useState<ISomeData>();
   useEffect(() => {
     fetch('/getSomeData')
     .then(res => res.json())
-    .then(json => setData(json));
+    .then((json: ISomeData) => setData(json));
   },[setData]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {`This data was fetched from an API: ${data}`}
+          {`This data was fetched from an API: ${data?.data} and number: ${data?.number}`}
         </p>
       </header>
     </div>
   );
 }
 
-export default App;
-function setState(): [any, any] {
-  throw new Error('Function not implemented.');
-}
 
